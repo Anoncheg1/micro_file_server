7d5d2a01ddffab3911a712e3c5e5705b1cea1428
none <none>
2024-01-30 22:04:20 +0000

    replace screenshot and now version v0.1.1

--- micro_file_server/__main__.py
+++ micro_file_server/__main__.py
@@ -3,9 +3,9 @@
 # Copyright (c) 2024 Anoncheg1
 
 # Author: Anoncheg1
-# Keywords: filesharing, fileserver, httpserver
+# Keywords: filesharing, fileserver, httpserver, simplehttp
 # URL: https://github.com/Anoncheg1/pinyin-isearch
-# Version: 0.1
+# Version: 0.1.1
 # Requires: Flask >= 2.3.2
 
 # License:
@@ -222,7 +222,7 @@ def get_sizes(abs_path, files) -> Iterator[str]:
             while sz > 1024:
                 sz = sz / 1024
                 unit_idx += 1
-            size = f"{round(sz, 1)} {_SZ_UNITS[unit_idx]}"
+            size = f"{round(sz, 1):g} {_SZ_UNITS[unit_idx]}"
         else:
             size = '-'
         yield size
